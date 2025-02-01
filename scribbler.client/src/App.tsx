36839bd7a9acb13c62ac0c8178584f7dd1235ff2
Anthony <anthonydaniel123@live.com>
2025-02-01 15:11:45 -0400

    Finished basic layout

--- scribbler.client/src/App.tsx
+++ scribbler.client/src/App.tsx
@@ -4,17 +4,17 @@ function App() {
     return (
         <div className="container h-100">
             <div className="row h-100">
-                <div className="col-md-1 bg-success">
+                <div className="col-2 bg-success">
                     <p>Tool Bar</p>
                 </div>
-                <div className="col-md-11 h-100 bg-danger">
+                <div className="col-10 h-100 bg-danger">
                     <div className="d-flex justify-content-center h-100">
-                        <div className="d-flex gap-2 w-75 h-100 bg-warning h-300"> {/* Added h-300 class */}
-                            <div className="bg-dark h-auto"> {/* Added h-auto class */}
+                        <div className="d-flex gap-2 flex-column w-100 h-100 bg-warning"> {/* Add max to width */}
+                            <div className="bg-dark h-auto flex-grow-1"> 
 
                             </div>
-                            <div className="bg-dark h-auto"> {/* Added h-auto class */}
-
+                            <div className="bg-dark h-200 w-100">
+                                <canvas className="bg-light"></canvas>
                             </div>
                         </div>
                     </div>
