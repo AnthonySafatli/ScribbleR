d545479caa083fe687964d7a31fc095ae4856731
Anthony <anthonydaniel123@live.com>
2025-02-01 14:59:19 -0400

    feat: Add basic layout to the application

--- scribbler.client/src/App.tsx
+++ scribbler.client/src/App.tsx
@@ -1,9 +1,25 @@
 
 function App() {
     return (
-        <>
-            <h1>Hello!</h1>
-        </>
+        <div className="container h-100">
+            <div className="row h-100">
+                <div className="col-md-1 bg-success">
+                    <p>Tool Bar</p>
+                </div>
+                <div className="col-md-11 h-100 bg-danger">
+                    <div className="d-flex justify-content-center h-100">
+                        <div className="d-flex gap-2 w-75 h-100 bg-warning">
+                            <div className="bg-dark">
+                                
+                            </div>
+                            <div className="bg-dark">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
