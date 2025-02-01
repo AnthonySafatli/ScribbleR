339fa784ffddaed41e2a6a1a980ff4073072bd70
Anthony <anthonydaniel123@live.com>
2025-02-01 15:13:05 -0400

    Restarted to focus on functionality

--- scribbler.client/src/App.tsx
+++ scribbler.client/src/App.tsx
@@ -1,25 +1,8 @@
-import './App.css';
 
 function App() {
     return (
-        <div className="container h-100">
-            <div className="row h-100">
-                <div className="col-2 bg-success">
-                    <p>Tool Bar</p>
-                </div>
-                <div className="col-10 h-100 bg-danger">
-                    <div className="d-flex justify-content-center h-100">
-                        <div className="d-flex gap-2 flex-column w-100 h-100 bg-warning"> {/* Add max to width */}
-                            <div className="bg-dark h-auto flex-grow-1"> 
-
-                            </div>
-                            <div className="bg-dark h-200 w-100">
-                                <canvas className="bg-light"></canvas>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div>
+            hi
         </div>
     );
 }
