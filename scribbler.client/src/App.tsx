6fba679a75d3d50026107059536faab6c2ac9652
Anthony <anthonydaniel123@live.com>
2025-02-03 20:07:56 -0400

    feat: Add Send and Clear buttons to the canvas

--- scribbler.client/src/App.tsx
+++ scribbler.client/src/App.tsx
@@ -1,11 +1,22 @@
+import './App.css'
+import ClearButton from './components/ClearButton';
+
 import DrawCanvas from "./components/DrawCanvas";
+import SendButton from './components/SendButton';
 import ToolBar from "./components/ToolBar";
 
 function App() {
     return (
-        <div>
-            <ToolBar />
-            <DrawCanvas />
+        <div className="container">
+            <div className="d-flex gap-2">
+                {/* <ToolBar /> */}
+                <DrawCanvas />
+
+                <div className="d-flex gap-2 ">
+                    <SendButton />
+                    <ClearButton />
+                </div>
+            </div>
         </div>
     );
 }
